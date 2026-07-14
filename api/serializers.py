f6a587ba8324136cb78c64f741a1119fe4ea8ed2
dare-web495 <consoledaleanto@gmail.com>
2026-07-14 21:43:14 +0300

    Finished serializers.py completely

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,27 @@ from .models import Checkin, Habit, User, Category
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CheckinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Checkin
+        fields = '__all__'
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    category = CategorySerializer(many=True, read_only=True)
+    checkin_set = CheckinSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Habit
+        fields = ['user', 'habit_name', 'frequency', 'created_date', 'category', 'checkin_set']
