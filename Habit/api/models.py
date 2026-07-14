0b5caddbb670e532a89d2dc3698696076bcd30f1
dare-web495 <consoledaleanto@gmail.com>
2026-07-14 16:54:26 +0300

    finishing setting up our Models

--- Habit/api/models.py
+++ Habit/api/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class Habit(models.Model):
+    FREQUENCY_CHOICES = [
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    habit_name = models.CharField(max_length=50)
+    frequency = models.CharField(max_length=10, choices=FREQUENCY_CHOICES, default='daily')
+    created_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, related_name='habit')
+    
+    def __str__(self):
+        return f"{self.user.username} has {self.habit_name} at {self.created_date}"
+    
+
+
+class Checkin(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['habit', 'date'],
+                name='prevent_double_checkin'
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.habit.user.username} checked in at {self.date}"
+    
+    
