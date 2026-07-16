97847ed58be0e48bd2823efa85cc9bd9f0c07868
dare-web495 <consoledaleanto@gmail.com>
2026-07-16 10:55:17 +0300

    Now setting up views.py and working on it

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Habit, Category, User, Checkin
+from .serializers import UserSerializer, CheckinSerializer, HabitSerializer, CategorySerializer
 
 # Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
