12f684932a8b85f7b5dfcdea81ecc406c0d876c2
dare-web495 <consoledaleanto@gmail.com>
2026-07-16 15:53:43 +0300

    Finished up on our viewsets

--- api/views.py
+++ api/views.py
@@ -7,3 +7,18 @@ from .serializers import UserSerializer, CheckinSerializer, HabitSerializer, Cat
 class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+
+class CheckinViewset(viewsets.ModelViewSet):
+    queryset = Checkin.objects.all()
+    serializer_class = CheckinSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
