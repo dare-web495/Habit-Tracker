0090d69ef0f545013fa73bab9fc1a7f787c08e88
dare-web495 <consoledaleanto@gmail.com>
2026-07-17 07:03:34 +0300

    working on permissions now

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from rest_framework.router import DefaultRouter
+from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
 
-router.regiser(r'habit', views.HabitViewSet, basename='habit')
-router.regiser(r'category', views.CategoryViewSet, basename='category')
-router.regiser(r'checkin', views.CheckinViewset, basename='checkin')
+router.register(r'habit', views.HabitViewSet, basename='habit')
+router.register(r'category', views.CategoryViewSet, basename='category')
+router.register(r'checkin', views.CheckinViewset, basename='checkin')
 
 urlpatterns = [
     path('', include(router.urls))
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Habit, Category, Checkin
 from .serializers import CheckinSerializer, HabitSerializer, CategorySerializer
 
@@ -7,13 +8,23 @@ from .serializers import CheckinSerializer, HabitSerializer, CategorySerializer
 class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Habit.objects.filter(self.request.user)
 
 class CheckinViewset(viewsets.ModelViewSet):
     queryset = Checkin.objects.all()
     serializer_class = CheckinSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Checkin.objects.filter(self.request.user)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(self.request.user)
